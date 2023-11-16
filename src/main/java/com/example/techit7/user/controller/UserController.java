95593f47a3b7dcf1973318234503023d4b0b168e
tallguy188 <cksghd4220@naver.com>
2023-11-16 17:22:52 +0900

    feat: 기본경로생성

--- src/main/java/com/example/techit7/user/controller/UserController.java
+++ src/main/java/com/example/techit7/user/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.example.techit7.user.controller;
 
+import com.example.techit7.user.dto.UserRequestDto;
 import com.example.techit7.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RequiredArgsConstructor
@@ -11,7 +17,18 @@ public class UserController {
 
     private final UserService userService;
 
+    @PostMapping("/member/login")
+    public String login(@RequestBody UserRequestDto userRequestDto){
+        return "";
+    }
 
+    @PostMapping("/member/signup")
+    public String postUser(@RequestBody UserRequestDto userRequestDto) {
+        return "";
+    }
 
-
+    @PatchMapping("/member/{userid}")
+    public String changeNickname(@PathVariable String userid){
+        return "";
+    }
 }
