5c99e3d0c53433d8a6984000ce32ac1534636ced
tallguy188 <cksghd4220@naver.com>
2023-11-17 17:26:14 +0900

    feat: password 암호화 과정 추가

--- src/main/java/com/example/techit7/user/service/UserServiceImpl.java
+++ src/main/java/com/example/techit7/user/service/UserServiceImpl.java
@@ -12,6 +12,7 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpRequest;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.net.http.HttpResponse;
@@ -22,6 +23,7 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
+    private final BCryptPasswordEncoder encoder;
 
     @Override
     public User getUserById(Long id) {
@@ -40,11 +42,9 @@ public class UserServiceImpl implements UserService {
         User user = userRepository.findByLoginId(userRequestDto.getLoginId())
                 .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 유저입니다."));
 
-        String receivePw = userRequestDto.getPassword();
-
-        if(receivePw != user.getPassword()){
+        if(!encoder.matches(userRequestDto.getPassword(),user.getPassword())){
             // 로그인 실패
-            return false;
+            return false;  // 추후에 예외 추가
         }
 
         // 쿠키 및 세션 설정
@@ -58,6 +58,11 @@ public class UserServiceImpl implements UserService {
     }
     @Override
     public boolean postUser(UserRequestDto userRequestDto) {
+        //  DTO값들의 널값 체크 필요
+        if(!userRequestDto.isDtoEntireVariableNull()){
+            return false;
+        }
+
         //닉네임 중복체크
         userRepository.findByNickname(userRequestDto.getNickName()).ifPresent(user -> {
             throw new RuntimeException();
@@ -69,13 +74,11 @@ public class UserServiceImpl implements UserService {
         });
 
         // 이메일 인증 (추후 추가)
-        // 비밀번호 암호화 (추후 추가)
-
         // 저장
         User user = userRepository.save(
                 User.builder()
                         .loginId(userRequestDto.getLoginId())
-                        .password(userRequestDto.getPassword())
+                        .password(encoder.encode(userRequestDto.getPassword()))  // 패스워드 암호화
                         .nickname(userRequestDto.getNickName())
                         .email(userRequestDto.getEmail())
                         .build());
