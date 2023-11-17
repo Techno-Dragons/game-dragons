aef3fa4a0d752cbece34ccb8294af3c07f9ba2a7
tallguy188 <cksghd4220@naver.com>
2023-11-17 17:25:43 +0900

    feat: null값 체크 함수 추가

--- src/main/java/com/example/techit7/user/dto/UserRequestDto.java
+++ src/main/java/com/example/techit7/user/dto/UserRequestDto.java
@@ -6,6 +6,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.lang.reflect.Field;
+
 @Getter
 @NoArgsConstructor
 public class UserRequestDto {
@@ -14,4 +16,17 @@ public class UserRequestDto {
     private String password;
     private String nickName;
     private String email;
+
+    public boolean isDtoEntireVariableNull() {
+        try {
+            for (Field f : getClass().getDeclaredFields()) {
+                if (f.get(this) != null) {
+                    return false;
+                }
+            }
+            return true;
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
