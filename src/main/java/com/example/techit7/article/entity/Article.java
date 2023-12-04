72c11d7a7cd98d3f85e630108ba3ceb6c991747c
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2023-12-05 00:16:12 +0900

    refactor: dto 멤버 null체크
    modifyArticle Dto멤버 null 체크
    
    Ref: #2

--- src/main/java/com/example/techit7/article/entity/Article.java
+++ src/main/java/com/example/techit7/article/entity/Article.java
@@ -4,13 +4,23 @@ import com.example.techit7.article.dto.ArticleRequestDto;
 import com.example.techit7.comment.Comment;
 import com.example.techit7.user.User;
 import com.example.techit7.util.DateTime;
-import jakarta.persistence.*;
-
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.util.StringUtils;
 
 @Entity
 @Getter
@@ -66,9 +76,16 @@ public class Article extends DateTime{
 
     //게시글 수정
     public void modifyArticle(ArticleRequestDto articleRequestDto) {
-        this.title = articleRequestDto.getTitle();
-        this.content = articleRequestDto.getContent();
-        this.category = articleRequestDto.getCategory();
+        if (StringUtils.hasText(articleRequestDto.getTitle())) {
+            this.title = articleRequestDto.getTitle();
+        }
+        if (StringUtils.hasText(articleRequestDto.getContent())) {
+            this.title = this.content = articleRequestDto.getContent();
+        }
+        if (StringUtils.hasText(articleRequestDto.getCategory())) {
+            this.title = this.category = articleRequestDto.getCategory();
+        }
+
     }
 
 }
