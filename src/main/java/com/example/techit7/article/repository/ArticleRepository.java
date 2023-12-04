694a19b7096361b1d16038e7438dea7eafef0bbd
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2023-12-05 00:19:23 +0900

    feat: Article CRUD, Image CRD 기능 추가
    articleAll, createArticle, deleteArticle, modifyArticle
    
    imageView
    
    Ref: #5

--- src/main/java/com/example/techit7/article/repository/ArticleRepository.java
+++ src/main/java/com/example/techit7/article/repository/ArticleRepository.java
@@ -2,8 +2,6 @@ package com.example.techit7.article.repository;
 
 import com.example.techit7.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
