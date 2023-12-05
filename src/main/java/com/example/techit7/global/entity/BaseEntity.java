5a11b9e2d6a437f4f4f5b936c7f459dd18dc6a84
Bisi3asi <kueeng8008@naver.com>
2023-12-05 17:18:14 +0900

    # 제목: 아래에 작성, 제목 끝 마침표 금지, 개조식 작성(과거형 금지) # [형식]: <타입>: <제목> 형식으로 작성 (최대 50글자, : 뒤에만 공백) ################(제목 시작) fix: (BaseEntity) toBuilder 추가 ################(제목 끝) ################(제목, 본문 한 줄 공백, 작성 금지)
    ################(제목, 본문 한 줄 공백, 작성 금지)
    # 본문(추가 설명): 아래에 작성 (한 줄당 최대 72글자)
    # 무엇을 왜 변경했는지 상세히 설명
    ################(본문 시작)
    
    ################(본문 끝)
    # 꼬릿말(footer): 아래에 작성 (관련 이슈 등 기재)
    # [형식]: <유형>: #<이슈 번호> 형식으로 작성, 다수의 이슈 번호는 ,로 구분
    ################(꼬릿말 시작)
    
    ################(꼬릿말 끝)
    # [참고]: Commit Type Rule
    # feature: 새로운 기능 추가
    # fix: 버그 수정
    # docs: 문서 수정
    # test: 테스트 코드 추가
    # refactor: 코드 리팩토링
    # rename: 파일 혹은 폴더 수정, 이동 작업 수행
    # remove: 파일 삭제 작업 수행
    # comment: 주석 추가 또는 변경
    # design: CSS 등 사용자 UI 디자인 변경
    # style: 코드 의미에 영향을 주지 않는 변경사항
    # chore: 빌드 부분 혹은 패키지 매니저 수정사항
    # init: 프로젝트 초기 생성
    ################
    # [참고]: Footer Rule
    # fixes: 이슈 수정 중(미해결)
    # Resolves: 이슈 해결
    # Ref: 참조할 이슈가 있을 때
    # Related to: 해당 커밋에 관련된 이슈 번호(미해결)

--- src/main/java/com/example/techit7/global/entity/BaseEntity.java
+++ src/main/java/com/example/techit7/global/entity/BaseEntity.java
@@ -8,12 +8,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -22,7 +18,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Getter
 @ToString
 @Embeddable
-@SuperBuilder
+@SuperBuilder(toBuilder = true)
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
