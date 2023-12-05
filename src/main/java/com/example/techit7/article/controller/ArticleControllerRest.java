1f0eff796b1ae905249235dbdfade79d3fa2c13e
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2023-12-05 17:43:31 +0900

    # 제목: 아래에 작성, 제목 끝 마침표 금지, 개조식 작성(과거형 금지) # [형식]: <타입>: <제목> 형식으로 작성 (최대 50글자, : 뒤에만 공백) ################(제목 시작) fix: (Comment) @SuperBuilder(toBuilder) 추가 ################(제목 끝) ################(제목, 본문 한 줄 공백, 작성 금지) remove: ArticleController 테스트 코드 삭제 ################(제목, 본문 한 줄 공백, 작성 금지) # 본문(추가 설명): 아래에 작성 (한 줄당 최대 72글자) # 무엇을 왜 변경했는지 상세히 설명 ################(본문 시작) 테스트 용으로 사용한 ArticleControllerRest를 전부 주석처리 ################(본문 끝) # 꼬릿말(footer): 아래에 작성 (관련 이슈 등 기재) # [형식]: <유형>: #<이슈 번호> 형식으로 작성, 다수의 이슈 번호는 ,로 구분 ################(꼬릿말 시작)
    ################(꼬릿말 끝)
    # [참고]: Commit Type Rule
    # feature: 새로운 기능 추가
    # fix: 버그 수정
    # docs: 문서 수정
    # test: 테스트 코드 추가
    # refactor: 코드 리팩토링
    # rename: 파일 혹은 폴더 수정, 이동 작업 수행
    # remove: 파일 삭제 작업 수행
    # comment: 주석 추가 또는 변경
    # design: CSS 등 사용자 UI 디자인 변경
    # style: 코드 의미에 영향을 주지 않는 변경사항
    # chore: 빌드 부분 혹은 패키지 매니저 수정사항
    # init: 프로젝트 초기 생성
    ################
    # [참고]: Footer Rule
    # fixes: 이슈 수정 중(미해결)
    # Resolves: 이슈 해결
    # Ref: 참조할 이슈가 있을 때
    # Related to: 해당 커밋에 관련된 이슈 번호(미해결)

---
+++ src/main/java/com/example/techit7/article/controller/ArticleControllerRest.java
@@ -0,0 +1,105 @@
+//package com.example.techit7.article.controller;
+//
+//
+//import com.example.techit7.article.dto.ArticleRequestDto;
+//import com.example.techit7.article.dto.ArticleResponseDto;
+//import com.example.techit7.article.service.ArticleServiceImpl;
+//import com.example.techit7.article.service.ImageService;
+//import java.io.IOException;
+//import java.net.MalformedURLException;
+//import java.security.Principal;
+//import java.util.List;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.core.io.Resource;
+//import org.springframework.core.io.UrlResource;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.DeleteMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PatchMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestPart;
+//import org.springframework.web.bind.annotation.ResponseStatus;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//@RestController
+//@RequiredArgsConstructor
+//public class ArticleControllerRest {
+//
+//    private final ArticleServiceImpl articleService;
+//    private final ImageService imageService;
+//
+//    // Article 전체 출력
+//    @GetMapping("/article")
+//    public ResponseEntity<List<ArticleResponseDto>> articleAll(ArticleResponseDto articleResponseDto
+////                                                               @RequestBody(required = false) ArticleRequestDto articleRequestDto,
+////                                                               @RequestParam String mode
+//    ) {
+////        if (mode.equals("write")) {
+////
+////        }
+//        List<ArticleResponseDto> articleResponseDtos = articleService.getArticles();
+//        return ResponseEntity.ok(articleResponseDtos);
+//    }
+//
+//    // Article 저장
+//    @ResponseStatus(HttpStatus.CREATED)
+//    @PostMapping("/article")
+//    public void createArticle(@RequestPart ArticleRequestDto articleRequestDto,
+//                              @RequestPart(value = "file", required = false) MultipartFile multipartFile,
+//                              Principal principal) throws IOException {
+//
+//        Long articleId = articleService.postArticle(articleRequestDto, null);
+//
+//        imageService.save(multipartFile, articleId);
+//    }
+//
+//    // Article 단일 출력
+//    @GetMapping("/article/{id}")
+//    public ResponseEntity<ArticleResponseDto> detailArticle(@PathVariable Long id,
+//                                                            @RequestParam String mode,
+//                                                            ArticleRequestDto articleRequestDto) {
+////        if (mode.equals("modify")) {
+////            articleService.updateArticleById(id, articleRequestDto);
+////        }
+////        if (mode.equals("delete")) {
+////            articleService.deleteArticleById(id);
+////            return ResponseEntity.noContent()
+////        }
+//
+//        ArticleResponseDto articleResponseDto = articleService.getArticleById(id);
+//
+//        return ResponseEntity.ok(articleResponseDto);
+//    }
+//
+//    // Article 삭제
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
+//    @DeleteMapping("/article/{id}")
+//    public void deleteArticle(@PathVariable Long id) {
+//        imageService.delete(id);
+//        articleService.deleteArticleById(id);
+//    }
+//
+//    // Article 수정
+//    @ResponseStatus(HttpStatus.OK)
+//    @PatchMapping("/article/{id}")
+//    public void modifyArticle(@PathVariable Long id,
+//                              @RequestBody ArticleRequestDto articleRequestDto) {
+//
+//        articleService.updateArticleById(id, articleRequestDto);
+//    }
+//
+//
+//    // Image 출력
+//    @GetMapping("/article/image")
+//    public ResponseEntity<Resource> imageView(@RequestParam Long articleId) throws MalformedURLException {
+//        String fullPathFilename = imageService.getFullPathStoreFilenameByArticleId(articleId);
+//
+//        return ResponseEntity.ok(new UrlResource("file:" + fullPathFilename));
+//    }
+//
+//}
