08e1f55b667cd2abee1129ae62b6e015357283cf
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2023-12-06 17:00:12 +0900

    feat: GlobalResponseDto 상속 추가

--- src/main/java/com/example/techit7/article/dto/ImageResponseDto.java
+++ src/main/java/com/example/techit7/article/dto/ImageResponseDto.java
@@ -1,6 +1,7 @@
 package com.example.techit7.article.dto;
 
 import com.example.techit7.article.entity.Article;
+import com.example.techit7.global.dto.GlobalResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,7 +11,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class ImageResponseDto {
+public class ImageResponseDto extends GlobalResponseDto<ImageResponseDto> {
     private Long id;
     
     private Article article;
