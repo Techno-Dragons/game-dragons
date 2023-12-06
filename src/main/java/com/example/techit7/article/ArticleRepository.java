5744661576285f9153a83a715168f536e9ea2bd0
yonguk <kimyu9827@naver.com>
2023-12-06 17:26:00 +0900

    fix: conflict file remove;

--- src/main/java/com/example/techit7/article/ArticleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.techit7.article;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-}
