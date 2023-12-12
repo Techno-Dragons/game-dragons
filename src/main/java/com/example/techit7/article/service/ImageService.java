a0ca767e2c282b181faf66a0f74f7f67c4af1b3f
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2023-12-13 01:33:44 +0900

    fix: 이미지 없을 경우에도 이상하게 출력되는 오류 수정

--- src/main/java/com/example/techit7/article/service/ImageService.java
+++ src/main/java/com/example/techit7/article/service/ImageService.java
@@ -27,7 +27,7 @@ public class ImageService {
     @Transactional
     public void save(MultipartFile multipartFile, Long articleId) throws IOException {
 
-        if (multipartFile == null) {
+        if (multipartFile == null || multipartFile.isEmpty()) {
             return;
         }
 
@@ -76,7 +76,7 @@ public class ImageService {
     public GlobalResponseDto<ImageResponseDto> getByArticleId(Long articleId) {
         Optional<Image> image = imageRepository.findByArticleId(articleId);
         if (image.isEmpty()) {
-            throw new EntityNotFoundException(ErrorMessage.ENTITY_NOT_FOUND);
+            return null;
         }
 
         return GlobalResponseDto.of("200", "200", ImageResponseDto.builder()
