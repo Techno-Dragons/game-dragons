8146654c2f82aa858986bd907a9bd6a6c7c7696e
yonguk98 <84844598+yonguk98@users.noreply.github.com>
2023-12-13 16:30:25 +0900

    User (#16)
    * init: new branch user
    
    * fix: return template path change
    
    
    반환하는 템플릿의 경로를 현재 디렉토리에 맞게 수정
    
    * init: new file from dev pull
    
    * feature: login form template
    
    
    부트스트랩 적용 테스트중, 임시 레이아웃파일 생성
    
    * feature: adapt bootstram to signup form
    
    
    회원가입 페이지에 부트스트랩 적용, 닉네임 값이 널값으로 들어가는 문제 해결중
    
    * feature: new file add
    
    
    로그아웃 없이 다른 아이디로 로그인 할 때 스프링 시큐리티 UserDetail Bean 중복 등록으로 인한 stackoverflow 발생.
    해결을 위해 UserDetailService파일을 커스텀설정.
    
    * fix: UserController mypage, parameter add
    
    
    마이페이지 뷰 템플릿에 전달할 userCreateRequestDto 추가
    
    * fix: transational add
    
    
    UserSecurityService에서 발생한 transactional 이슈 해결을 위해 어노테이션 추가
    
    * feature: mypage_form adapt bootstrap
    
    
    마이페이지 템플릿에 부트스트랩 적용 및 TODO 주석 제거
    
    * fix: password encoding
    
    
    비밀번호 변경시 암호화 적용
    
    * style: layout path change

--- src/main/java/com/example/techit7/user/repository/UserRepository.java
+++ src/main/java/com/example/techit7/user/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.example.techit7.user.repository;
 import com.example.techit7.user.entity.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -15,6 +16,7 @@ public interface UserRepository extends JpaRepository<SiteUser, Long> {
 
     Optional<SiteUser> findByPassword(String password);
 
+    @Transactional(readOnly = true)
     Optional<SiteUser> findByUsername(String username);
 
     Optional<SiteUser> findByEmail(String email);
