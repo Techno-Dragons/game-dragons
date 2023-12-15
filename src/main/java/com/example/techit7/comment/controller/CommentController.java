e2beeda855718ed9a02db28cacb75dd35e4290e8
Bisi3asi <kueeng8008@naver.com>
2023-12-15 16:21:14 +0900

    fix, design: Validation, design 수정

--- src/main/java/com/example/techit7/comment/controller/CommentController.java
+++ src/main/java/com/example/techit7/comment/controller/CommentController.java
@@ -13,11 +13,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/article/{articleId}/comment")
@@ -51,7 +47,7 @@ public class CommentController {
 
     //PUT :/article/{articleId}/comment/{commentId}
     @PreAuthorize("isAuthenticated()")
-    @PutMapping("/{commentId}")
+    @PostMapping("/{commentId}")
     public String modify(
             @PathVariable("articleId") Long articleId,
             @PathVariable("commentId") Long commentId,
