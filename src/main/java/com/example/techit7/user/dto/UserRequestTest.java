2973444e03578286da3bf5f6e6a9fa6f23c0332c
yonguk <kimyu9827@naver.com>
2023-12-17 22:23:45 +0900

    feature: user signup test
     api 통신 테스트 및 회원가입 테스트 완료

--- src/main/java/com/example/techit7/user/dto/UserRequestTest.java
+++ src/main/java/com/example/techit7/user/dto/UserRequestTest.java
@@ -1,2 +1,18 @@
-package com.example.techit7.user.dto;public class UserRequestTest {
+package com.example.techit7.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class UserRequestTest {
+
+    private String username;
+    private String password1;
+    private String password2;
+    private String email;
+    private String nickname;
 }
