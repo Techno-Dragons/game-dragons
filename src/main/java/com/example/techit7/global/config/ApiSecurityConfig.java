ac2391da64f8ae0cc72f78594f202b073654182b
yonguk98 <84844598+yonguk98@users.noreply.github.com>
2023-12-17 22:36:49 +0900

    User rest (#23)
    * init: new branch user
    
    * fix: return template path change
    
    
    반환하는 템플릿의 경로를 현재 디렉토리에 맞게 수정
    
    * init: new file from dev pull
    
    * feature: login form template
    
    
    부트스트랩 적용 테스트중, 임시 레이아웃파일 생성
    
    * feature: adapt bootstram to signup form
    
    
    회원가입 페이지에 부트스트랩 적용, 닉네임 값이 널값으로 들어가는 문제 해결중
    
    * feature: new file add
    
    
    로그아웃 없이 다른 아이디로 로그인 할 때 스프링 시큐리티 UserDetail Bean 중복 등록으로 인한 stackoverflow 발생.
    해결을 위해 UserDetailService파일을 커스텀설정.
    
    * fix: UserController mypage, parameter add
    
    
    마이페이지 뷰 템플릿에 전달할 userCreateRequestDto 추가
    
    * fix: transational add
    
    
    UserSecurityService에서 발생한 transactional 이슈 해결을 위해 어노테이션 추가
    
    * feature: mypage_form adapt bootstrap
    
    
    마이페이지 템플릿에 부트스트랩 적용 및 TODO 주석 제거
    
    * fix: password encoding
    
    
    비밀번호 변경시 암호화 적용
    
    * style: layout path change
    
    * feature: security file add for rest api
    
    
     rest api 적용을 위한 시큐리티 설정 파일 추가
    
    * feature: user signup test
    
    
     api 통신 테스트 및 회원가입 테스트 완료
    
    * chore: jjwt dependency add
    
    * rename: globlaResponseDto rename to globalResponse
    
    
    GlobalResponseDto에서 Dto 제거

---
+++ src/main/java/com/example/techit7/global/config/ApiSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.techit7.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class ApiSecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher("/api/**")
+                .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+        ;
+        return http.build();
+    }
+}
