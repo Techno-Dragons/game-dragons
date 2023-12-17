d15b256ad11e3dab07c8b1bbf9ddb04ad3f7d5e6
JB3 <60452780+Bisi3asi@users.noreply.github.com>
2023-12-17 22:26:47 +0900

    refactor: comment 도메인 rest api 리팩토링 (#22)

--- src/main/java/com/example/techit7/comment/service/CommentService.java
+++ src/main/java/com/example/techit7/comment/service/CommentService.java
@@ -2,6 +2,7 @@ package com.example.techit7.comment.service;
 
 import com.example.techit7.article.entity.Article;
 import com.example.techit7.comment.dto.CommentRequestDto;
+import com.example.techit7.comment.dto.CommentResponseDto;
 import com.example.techit7.comment.entity.Comment;
 
 import com.example.techit7.global.dto.GlobalResponseDto;
@@ -11,10 +12,9 @@ public interface CommentService {
 
     Comment get(Long id);
 
-    GlobalResponseDto<Comment> post(SiteUser siteUser, Article article, CommentRequestDto req);
+    CommentResponseDto post(SiteUser siteUser, Article article, CommentRequestDto req);
 
-    GlobalResponseDto<Comment> update(SiteUser siteUser, Long id, CommentRequestDto req);
+    CommentResponseDto update(SiteUser siteUser, Long id, CommentRequestDto req);
 
-    GlobalResponseDto<Comment> delete(SiteUser siteUser, Long id);
-
+    void delete(SiteUser siteUser, Long id);
 }
