0b39093c00cfd57c52657b1ef78a1c44c38f883f
yonguk <kimyu9827@naver.com>
2023-12-19 03:55:26 +0900

    refactoring: 불필요한 import 제거

--- src/main/java/com/example/techit7/user/controller/MemberRestController.java
+++ src/main/java/com/example/techit7/user/controller/MemberRestController.java
@@ -11,9 +11,7 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
