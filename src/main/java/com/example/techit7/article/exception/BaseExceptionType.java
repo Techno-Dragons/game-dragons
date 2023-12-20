67fd71f0bccd6b5be738f946c7cd37b35eaaf659
yonguk98 <84844598+yonguk98@users.noreply.github.com>
2023-12-20 21:56:45 +0900

    User rest (#26)
    * init: new branch user
    
    * fix: return template path change
    
    
    반환하는 템플릿의 경로를 현재 디렉토리에 맞게 수정
    
    * init: new file from dev pull
    
    * feature: login form template
    
    
    부트스트랩 적용 테스트중, 임시 레이아웃파일 생성
    
    * feature: adapt bootstram to signup form
    
    
    회원가입 페이지에 부트스트랩 적용, 닉네임 값이 널값으로 들어가는 문제 해결중
    
    * feature: new file add
    
    
    로그아웃 없이 다른 아이디로 로그인 할 때 스프링 시큐리티 UserDetail Bean 중복 등록으로 인한 stackoverflow 발생.
    해결을 위해 UserDetailService파일을 커스텀설정.
    
    * fix: UserController mypage, parameter add
    
    
    마이페이지 뷰 템플릿에 전달할 userCreateRequestDto 추가
    
    * fix: transational add
    
    
    UserSecurityService에서 발생한 transactional 이슈 해결을 위해 어노테이션 추가
    
    * feature: mypage_form adapt bootstrap
    
    
    마이페이지 템플릿에 부트스트랩 적용 및 TODO 주석 제거
    
    * fix: password encoding
    
    
    비밀번호 변경시 암호화 적용
    
    * style: layout path change
    
    * feature: security file add for rest api
    
    
     rest api 적용을 위한 시큐리티 설정 파일 추가
    
    * feature: user signup test
    
    
     api 통신 테스트 및 회원가입 테스트 완료
    
    * chore: jjwt dependency add
    
    * rename: globlaResponseDto rename to globalResponse
    
    
    GlobalResponseDto에서 Dto 제거
    
    * rename: GlobalResponseDto import path and file name change
    
    
    globla response dto 이름 변경에 따른 관련 함수들 변경
    
    * rename: user -> member
    
    * feature: signup, login
    
    * feature: signup, login dto add
    
    * comment: login TODO
    
    * feature: logout
    
    * refactoring: 불필요한 함수 제거
    
    * refactoring: 불필요한 import 제거
    
    * feature: logout, refresh accessToken
    
    
    로그아웃 기능 구현 완료, access token 재발급 기능 추가
    
    * comment: TODO remove
    
    * feature: jwt filter set authority
    
    * fix: jwt filter, find access token
    
    
    request header에서 X-ApiKey를 찾는 대신 쿠키에서 access token을 찾도록 변경
    
    * test: principal test
    
    principal 작동 테스트 함수 추가
    
    * feature: sign up, username duplication check
    
    
    회원가입 과정에서 유저이름 중복이면 예외 발생하도록 변경
    
    * fix: sign up validation
    
    
    유저 이름 중복 체크 로직 수정.
    
    * fix: NPE in refreshAccessToken
    
    
    NPE 문제 해결, optional 쿠키가 널값일 때를 확인하여  에러코드를 담은 응답 반환하도록 변경
    
    * refactoring: check for request.getCookies()
    
    
    getCookie()가 null 값을 반환하는지 확인하는 과정 추가
    
    * refactoring: return http status change
    
    
    중복 데이터 -> 409
    쿠키 없음, 쿠키의 데이터 없음 -> 401
    
    * refactoring: jsession cookie
    
    
    세션 쿠키가 전송되지 않도록 securityConfig 및 application.yml에 stateless로 설정하는 코드 추가
    
    * comment: TODO 추가
    
    * style:

--- src/main/java/com/example/techit7/article/exception/BaseExceptionType.java
+++
@@ -1,8 +0,0 @@
-package com.example.techit7.article.exception;
-
-import org.springframework.http.HttpStatus;
-
-public interface BaseExceptionType {
-    HttpStatus getHttpStatus();
-    String getErrorMessage();
-}
