733743be2a68e5f12c4e3c7cc76ead5073d128c8
yonguk98 <84844598+yonguk98@users.noreply.github.com>
2023-12-20 22:17:29 +0900

    Revert "User rest (#28)" (#29)
    This reverts commit 8cf592befed2d6187cbe6031f48cbd6af5e5c665.

---
+++ src/main/java/com/example/techit7/article/dto/ImageResponseDto.java
@@ -0,0 +1,22 @@
+package com.example.techit7.article.dto;
+
+import com.example.techit7.article.entity.Article;
+import com.example.techit7.global.response.GlobalResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ImageResponseDto extends GlobalResponseDto<ImageResponseDto> {
+    private Long id;
+    
+    private Article article;
+
+    private String storeFilename;
+
+
+}
---
+++ src/main/java/com/example/techit7/article/exception/BaseExceptionType.java
@@ -0,0 +1,8 @@
+package com.example.techit7.article.exception;
+
+import org.springframework.http.HttpStatus;
+
+public interface BaseExceptionType {
+    HttpStatus getHttpStatus();
+    String getErrorMessage();
+}
