a5b7b1d7d2f2c30e06f08332da5014e30b5d3228
KissShot <kissshot.heartunderblade1104@gmail.com>
2023-12-23 16:49:18 +0900

    feat: ExceptioHandler 추가

--- src/main/java/com/example/techit7/article/exception/ExceptionController.java
+++ src/main/java/com/example/techit7/article/exception/ExceptionController.java
@@ -2,6 +2,7 @@ package com.example.techit7.article.exception;
 
 
 import com.example.techit7.global.response.GlobalResponse;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -17,6 +18,18 @@ public class ExceptionController {
 
         GlobalResponse errorResponse = makeErrorResponse(e.getBindingResult());
         return new ResponseEntity<GlobalResponse>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<GlobalResponse> incorrectUserAccess(EntityNotFoundException e) {
+        GlobalResponse errorResponse = GlobalResponse.of("404", e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<GlobalResponse> userPermissionError(IllegalArgumentException e) {
+        GlobalResponse errorResponse = GlobalResponse.of("403", e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
     }
 
     private GlobalResponse makeErrorResponse(BindingResult bindingResult) {
