5137c8d4ad0cec612a88d0b2a4cce613d52d5543
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2024-01-05 16:53:55 +0900

    feat: 로그인 유무에 따른 뷰 변경

--- demo-repository/src/routes/article/+page.svelte
+++ demo-repository/src/routes/article/+page.svelte
@@ -2,7 +2,7 @@
 	import { page } from '$app/stores';
 	import axios from 'axios';
 	import { onMount } from 'svelte';
-	import WriteArticle from "./write/+page.svelte";
+	import WriteArticle from './write/+page.svelte';
 
 	let promise = Promise.resolve([]);
 	let pageList = $state({
@@ -12,6 +12,8 @@
 		totalElements: 0,
 		pageSize: 10
 	});
+
+	let memberInfo = null;
 
 	function login() {
 		return new Promise(async (resolve, reject) => {
@@ -37,6 +39,7 @@
 	let totalPages = $state();
 	let totalElements = $state();
 
+	let username = $state(null);
 	function formatDateTime(dateTimeString) {
 		const date = new Date(dateTimeString);
 		const options = {
@@ -107,209 +110,90 @@
 
 	onMount(() => {
 		loadArticleList(0);
-		login();
+
+		username = localStorage.getItem('username');
 	});
 </script>
 
 
-
-<!-- {#if isPost}
-	<div class="width-45 mr-auto ml-auto">
-		<textarea class="textarea textarea-ghost text-4xl w-full" placeholder="제목을 입력해주세요." bind:value={inputTitle} />
-		<div class="flex items-center space-x-2 mt-6" />
-		<div class="divider divider-Neutral mb-1" />
-
-		<div class="mt-8">
-			<textarea class="textarea textarea-ghost mt-1 w-full h-screen" placeholder="내용을 입력해주세요." bind:value={inputContent} />
-		</div>
-
-		<div class="flex space-x-4 mb-6 container flex-wrap flex-row-reverse">
-
-			<div class="flex-wrap">
-			<button
-				class="btn border-gray-600 btn-ghost"
-				on:click={() => {
-					postArticle();
-					inputTitle = "";
-					inputContent = "";
-					isPost = !isPost;
-				}}
-			>
-				작성 완료
-			</button>
-			<button
-				class="btn border-gray-600 btn-ghost "
-				on:click={() => {
-					inputTitle = "";
-					inputContent = "";
-					isPost = !isPost;
-				}}
-			>
-				취소
-			</button>
-		</div>
-		</div>
-	</div>
-{:else} -->
-	{#await promise}
-		<span class="justify-center items-center loading loading-bars loading-lg"></span>
-	{:then articles}
-		<div class="flex max-w-7xl mx-auto">
-			<div class="flex-1 p-8">
-				<div class="flex space-x-4 mb-6">
+{#await promise}
+	<span class="justify-center items-center loading loading-bars loading-lg"></span>
+{:then articles}
+	<div class="flex max-w-7xl mx-auto">
+		<div class="flex-1 p-8">
+			<div class="flex space-x-4 mb-6">
+				
+				{#if username}
+					{console.log('username : ', username)}
 					<button
 						class="btn border-gray-600 btn-ghost"
 						on:click={() => {
-							location.href="article/write";	
+							location.href = 'article/write';
 						}}>
 						글쓰기
 					</button>
-					<!-- <button
-						class="btn border-gray-600 btn-ghost"
-						on:click={() => {
-							isPost = !isPost;
-						}}
-					>
-						글쓰기
-					</button> -->
-					<!-- <button
-					class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2">
-					category2
-				</button>
-				<button
-					class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2">
-					category3
-				</button>
-				<button
-					class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2">
-					category4
-				</button>
-				<button
-					class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2">
-					category5
-				</button> -->
-				</div>
-				{#each pageList.articles as article}
-					<div
-						class="rounded-lg border bg-card text-card-foreground shadow-sm mb-6"
-						data-v0-t="card"
-					>
-						<div class="flex flex-col space-y-1.5 p-6">
-							<a href="http://localhost:5173/article/{article.id}">
-								<h3 class="text-2xl font-semibold leading-none tracking-tight">
-									{article.title}
-								</h3>
-								<p class="text-sm text-muted-foreground">
-									{article.content}
-								</p>
-							</a>
-						</div>
-						<div class="p-6">
-							<div class="flex justify-between items-center">
-								<div class="btn border-gray-600 btn-ghost">카테고리</div>
-								<span>{formatDateTime(article.createdTime)}</span>
-							</div>
+				{/if}
+			
+			</div>
+			{#each pageList.articles as article}
+				<div class="rounded-lg border bg-card text-card-foreground shadow-sm mb-6" data-v0-t="card">
+					<div class="flex flex-col space-y-1.5 p-6">
+						<a href="http://localhost:5173/article/{article.id}">
+							<h3 class="text-2xl font-semibold leading-none tracking-tight">
+								{article.title}
+							</h3>
+							<p class="text-sm text-muted-foreground">
+								{article.content}
+							</p>
+						</a>
+					</div>
+					<div class="p-6">
+						<div class="flex justify-between items-center">
+							<div class="btn border-gray-600 btn-ghost">카테고리</div>
+							<span>{formatDateTime(article.createdTime)}</span>
 						</div>
 					</div>
-				{:else}
-					<li>검색된 결과가 없습니다.</li>
-				{/each}
-			</div>
-		</div>
-	{:catch err}
-		<p style="color: red;">{err.message}</p>
-	{/await}
-
-	<!-- 페이지네이션 컨트롤 -->
-	<div class="grid place-items-center mb-10">
-		<div class="join">
-			<button
-				on:click={() => changePage(pageList.currentPage - 1)}
-				disabled={pageList.currentPage <= 0}
-				class="btn border-gray-600 btn-ghost mr-1"
-			>
-				이전
-			</button>
-			<!-- <span>페이지 {pageList.currentPage + 1} / {pageList.totalPages}</span> -->
-
-			{#each createPageArray(pageList.currentPage, pageList.totalPages) as page}
-				<input
-					on:click={() => changePage(page)}
-					class="join-item btn btn-square"
-					type="radio"
-					name="options"
-					aria-label={`Page ${page + 1}`}
-					checked
-				/>
+				</div>
+			{:else}
+				<li>검색된 결과가 없습니다.</li>
 			{/each}
-
-			<button
-				on:click={() => changePage(pageList.currentPage + 1)}
-				disabled={pageList.currentPage >= pageList.totalPages - 1}
-				class="btn border-gray-600 btn-ghost ml-1"
-			>
-				다음
-			</button>
 		</div>
 	</div>
-<!-- {/if} -->
-
-<!-- <form th:action="@{/article}" method="get" id="searchForm">
-			<input type="hidden" id="page" name="page" value={pageList.currentPage}>
-		</form> -->
-
-<!-- <script>
-	import axios from 'axios';
-	import '../app.css';
-	import { onMount } from 'svelte';
-
-	let promise = Promise.resolve([]);
-	let pageList = $state({
-		articles: [],
-		currentPage: 0,
-		totalPages: 0,
-		pageSize: 10
-	});
-
-	function loadArticleList() {
-		return new Promise(async (resolve, reject) => {
-			try {
-				const res = await axios.get('http://localhost:8090/article');
-				console.log(res);
-				pageList.articles = res.data.data.content;
-				pageList.totalPages = res.data.data.totalPages;
-				pageList.currentPage = res.data.data.number;
-				console.log(res.data.data.content);
-				resolve(pageList.articles);
-				console.log(pageList.articles[0].title);
-			} catch (err) {
-				console.log("err");
-				reject(err);
-			} finally {
-				console.log('done');
-			}
-		});
-	}
-
-	onMount(() => {
-		loadArticleList();
-	});
-	
-</script>
-
-  {#await promise}
-	<h1 style="color: royalblue;">loading...</h1>
-  {:then articles}
-	<ul>
-	  {#each articles as article}
-		<li>{article.title}</li>
-	  {:else}
-		<li>검색된 결과가 없습니다.</li>
-	  {/each}
-	</ul>
-  {:catch err}
+{:catch err}
 	<p style="color: red;">{err.message}</p>
-  {/await} -->
+{/await}
+
+<!-- 페이지네이션 컨트롤 -->
+<div class="grid place-items-center mb-10">
+	<div class="join">
+		<button
+			on:click={() => changePage(pageList.currentPage - 1)}
+			disabled={pageList.currentPage <= 0}
+			class="btn border-gray-600 btn-ghost mr-1"
+		>
+			이전
+		</button>
+
+		{#each createPageArray(pageList.currentPage, pageList.totalPages) as page}
+			<input
+				on:click={() => changePage(page)}
+				class="join-item btn btn-square"
+				type="radio"
+				name="options"
+				aria-label={`Page ${page + 1}`}
+				checked
+			/>
+		{/each}
+
+		<button
+			on:click={() => changePage(pageList.currentPage + 1)}
+			disabled={pageList.currentPage >= pageList.totalPages - 1}
+			class="btn border-gray-600 btn-ghost ml-1"
+		>
+			다음
+		</button>
+	</div>
+</div>
 
 <style>
 	.pagination button {
@@ -327,7 +211,7 @@
 
 	.flex-container {
 		display: flex;
-		align-items: center; /* 요소들을 수직 중앙에 정렬 */
+		align-items: center;
 	}
 	.width-45 {
 		width: 45%;
