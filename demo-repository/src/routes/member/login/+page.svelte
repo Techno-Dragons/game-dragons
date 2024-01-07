3990078d9f76b6ca9ff9b60bebe36dba5d3b30a7
ppusda <ppusda1234@gmail.com>
2024-01-07 21:23:03 +0900

    코드 최신화

--- demo-repository/src/routes/member/login/+page.svelte
+++ demo-repository/src/routes/member/login/+page.svelte
@@ -1,7 +1,10 @@
 <script>
     import {goto} from "$app/navigation";
     import {checkLogin} from "../login_check.js";
+    import {isLogin} from "$lib/login_stores.js";
+    import {toastNotice} from "../../../app.js";
 
+    checkLogin("로그인 상태 입니다.");
     $: username = '';
     $: password = '';
     async function login() {
@@ -16,19 +19,24 @@
                 password: password
             })
         })
-            .then((res) => res.json())
-            .then((res) => {
-                check_login(res);
+        .then((res) => res.json())
+        .then((res) => {
+            console.log(res.data);
+            if(check_login(res)){
                 localStorage.setItem('username', res.data.username);
                 localStorage.setItem('nickname', res.data.nickname);
-            });
-        goto('/');
+            }
+        });
+        isLogin.set(true);
+        await goto('/');
     }
 
     function check_login(res) {
-        if (res.data.accessToken) {
-            alert(res.msg);
+        toastNotice(res.msg);
+        if (res.data.username) {
+            return true;
         }
+        return false;
     }
 </script>
 
@@ -45,21 +53,21 @@
                 <div class="card-body p-1">
                     <label class="card-title" for="username">사용자ID</label>
                     <input
-                            class="textarea textarea-bordered"
-                            placeholder="username"
-                            type="text"
-                            id="username"
-                            bind:value={username}
+                        class="textarea textarea-bordered"
+                        placeholder="username"
+                        type="text"
+                        id="username"
+                        bind:value={username}
                     />
                 </div>
                 <div class="card-body p-1">
                     <label class="card-title" for="password">비밀번호</label>
                     <input
-                            class="textarea textarea-bordered"
-                            placeholder="password"
-                            type="password"
-                            id="password"
-                            bind:value={password}
+                        class="textarea textarea-bordered"
+                        placeholder="password"
+                        type="password"
+                        id="password"
+                        bind:value={password}
                     />
                 </div>
                 <button class="btn" type="submit" on:click={(event) => login()}>로그인</button>
