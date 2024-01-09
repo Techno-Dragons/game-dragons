9824a0d16be315d8e5597af6bbd822d0ba155e34
Bisi3asi <kueeng8008@naver.com>
2024-01-09 14:16:29 +0900

    fix: EnableConfigurationProperties -> ConfigurationPropertiesScan
    전역적으로 PropertiesScan 될 수 있도록 annotation 변경

--- src/main/java/com/example/techit7/Techit7Application.java
+++ src/main/java/com/example/techit7/Techit7Application.java
@@ -2,14 +2,12 @@ package com.example.techit7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import com.example.techit7.global.config.JwtUtil;
-
 @SpringBootApplication
+@ConfigurationPropertiesScan
 @EnableJpaAuditing
-@EnableConfigurationProperties(JwtUtil.class)
 public class Techit7Application {
     public static void main(String[] args) {
         SpringApplication.run(Techit7Application.class, args);
